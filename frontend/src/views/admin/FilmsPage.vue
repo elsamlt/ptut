3fc041f20cd70810042cf3d6da4997aefd1bd449
Elsa <144545254+milhetcreat@users.noreply.github.com>
2025-02-24 15:21:03 +0100

    page admin film

--- frontend/src/views/admin/FilmsPage.vue
+++ frontend/src/views/admin/FilmsPage.vue
@@ -1,51 +1,200 @@
 <template>
   <!-- Liste des films -->
-  <v-card class="film-card">
-    <v-row align="center">
-      <v-col cols="2">
-        <v-img src="https://via.placeholder.com/50" class="film-image"></v-img>
-      </v-col>
-      <v-col>
-        <span class="text-h6 font-weight-bold">2023</span>
-      </v-col>
-      <v-col cols="auto">
-        <v-btn icon color="purple">
-          <v-icon>mdi-delete</v-icon>
-        </v-btn>
-        <v-btn icon color="purple">
-          <v-icon>mdi-pencil</v-icon>
-        </v-btn>
-      </v-col>
-    </v-row>
-  </v-card>
+  <v-container v-if="!showEditFilm && !showAddFilm">
+    <FilmsCard @edit="openEditForm(selectedFilm)" @delete="handlerDelete(selectedFilm)"/>
+  </v-container>
+  <v-container v-if="showAddFilm">
+    <AddFilm @add="handleFilmAdded" @closeForm="showAddFilm = false"/>
+  </v-container>
+  <v-container v-if="showEditFilm">
+    <EditFilm :film="selectedFilm" @edit="handleFilmEdit" @cancel="showEditFilm = false" />
+    <EditMed
+      :med="selectedMed"
+      @edit="handleMedEdit"
+      @cancel="showEditMed = false"
+    />
+  </v-container>
 
   <!-- Bouton flottant pour ajouter un film -->
-  <v-btn class="add-btn" fab color="purple" dark>
-    <v-icon>mdi-plus</v-icon>
+  <v-btn v-if="!showAddFilm" class="btn add-btn" size="40px">
+    <v-icon class="icon" @click="showAddFilm = true; selectedFilm = null;">mdi-plus</v-icon>
   </v-btn>
+
+  <!-- Dialog de confirmation -->
+  <v-dialog v-model="dialogAdd" max-width="400px">
+    <v-card>
+      <v-card-text> Le film a été ajouté avec succès ! </v-card-text>
+      <v-card-actions>
+        <v-btn color="green" text @click="dialogAdd = false">OK</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <v-dialog v-model="dialogDelete" max-width="400px">
+    <v-card>
+      <v-card-text> Le film a été supprimé avec succès ! </v-card-text>
+      <v-card-actions>
+        <v-btn color="green" text @click="dialogDelete = false">OK</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <v-dialog v-model="dialogEdit" max-width="400px">
+    <v-card>
+      <v-card-text> Le film a été modifié avec succès ! </v-card-text>
+      <v-card-actions>
+        <v-btn color="green" text @click="dialogEdit = false">OK</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup>
+  import FilmsCard from "@/components/admin/FilmsCard.vue";
+  import EditFilm from "@/components/admin/EditFilm.vue";
+  import AddFilm from "@/components/admin/AddFilm.vue";
 
+  import { ref, onMounted, reactive } from "vue";
+
+  const url = "";
+  const listFilms = reactive([]);
+
+  const dialogAdd = ref(false);
+  const dialogDelete = ref(false);
+  const dialogEdit = ref(false);
+
+  const showEditFilm = ref(false);
+  const showAddFilm = ref(false);
+
+  const selectedFilm = ref(null);
+
+  const openEditForm = (film) => {
+    selectedFilm.value = { ...film }; // Cloner pour éviter la modification directe
+    showEditFilm.value = true;
+  };
+
+  /**
+   * Récupérer les films depuis l'API
+   */
+  function fetchFilms() {
+    fetch(url)
+      .then((response) => response.json())
+      .then((dataJSON) => {
+        listFilms.splice(0, listFilms.length, ...dataJSON);
+      })
+      .catch((error) =>
+        console.error("Erreur lors de la récupération des films :", error),
+      );
+  }
+
+  /**
+   * Sélectionner un film et afficher ses détails
+   */
+  function fetchFilmDetail(film) {
+    fetch(`${url}/${film.id}`)
+      .then((response) => response.json())
+      .then((dataJSON) => {
+        selectedFilm.value = dataJSON;
+        showAddFilm.value = false;
+      })
+      .catch((error) =>
+        console.error("Erreur lors de la récupération des films :", error),
+      );
+  }
+
+  /**
+   * Ajouter un nouveau film via API
+   */
+  const handleFilmAdded = (newFilm) => {
+    // Ajouter le film via l'API
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        titre: newFilm.titre,
+        synopsis: newFilm.synopsis,
+        genre: newFilm.genre,
+        annee: newFilm.annee,
+        duree: newFilm.duree,
+        affiche: newFilm.affiche,
+        urlFilm: newFilm.urlFilm,
+        urlBA: newFilm.urlBA,
+      }),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        fetchFilms(); // Rafraîchir la liste après l'ajout
+        // Afficher la popup de confirmation
+        dialogAdd.value = true;
+        showAddFilm.value = false;
+      })
+      .catch((error) =>
+        console.error("Erreur lors de l'ajout du film :", error),
+      );
+  };
+
+  /**
+   * Supprimer un film via API
+   */
+  function handlerDelete(film) {
+    fetch(`${url}/${film.id}`, { method: "DELETE" })
+      .then((response) => {
+        if (response.ok) fetchFilms();
+        dialogDelete.value = true;
+        selectedFilm.value = null;
+      })
+      .catch((error) => console.error("Erreur lors de la suppression :", error));
+  }
+
+  /**
+   * Modifier un film comme faite via API
+   */
+  const handleFilmEdit = (updatedFilm) => {
+    fetch(`${url}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        titre: updatedFilm.titre,
+        synopsis: updatedFilm.synopsis,
+        genre: updatedFilm.genre,
+        annee: updatedFilm.annee,
+        duree: updatedFilm.duree,
+        affiche: updatedFilm.affiche,
+        urlFilm: updatedFilm.urlFilm,
+        urlBA: updatedFilm.urlBA,
+      }),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        fetchFilmDetail(updatedFilm);
+        dialogEdit.value = true; // Afficher le message de confirmation
+        showEditFilm.value = false;
+      })
+      .catch((error) => console.error("Erreur lors de la mise à jour :", error));
+  };
+
+  // Charger les films au montage
+  onMounted(fetchFilms);
 </script>
 
 <style scoped>
-/* Carte du film */
-.film-card {
-  padding: 10px;
-  margin-top: 20px;
-}
-
-/* Image du film */
-.film-image {
-  width: 50px;
-  height: 75px;
-}
 
 /* Bouton flottant */
 .add-btn {
   position: fixed;
   bottom: 20px;
   right: 20px;
+}
+
+.btn {
+  background-color: var(--color-button);
+  color: var(--color-text);
+  border-radius: 50px;
+  box-shadow: inset 0 0 5px rgba(255, 255, 255, 0.6);
+  margin-right: 10px;
+}
+
+.btn:hover .icon {
+  transform: scale(0.8);
 }
 </style>
--- frontend/src/views/admin/IndexPage.vue
+++ frontend/src/views/admin/IndexPage.vue
@@ -1,11 +1,113 @@
-<script setup>
-
-</script>
-
 <template>
+  <div class="container">
+    <!-- Barre de navigation latérale -->
+      <v-list class="navigation-drawer" dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.link"
+          :to="item.link"
+          :class="{ 'active-item': $route.path === item.link }"
+        >
+          <template v-slot:prepend>
+            <v-icon>{{ item.icon }}</v-icon>
+          </template>
+          <v-list-item-title>{{ item.label }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
 
+    <hr class="custom-hr">
+
+    <!-- Contenu principal -->
+    <div class="active-page">
+      <h1 class="text-h5 font-weight-bold">ESPACE ADMINISTRATION</h1>
+      <p><strong>{{ currentTitle }}</strong></p>
+      <router-view />
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import { ref, computed } from "vue";
+import { useRoute } from "vue-router";
 
+const route = useRoute();
+
+const items = ref([
+  {
+    label: "Films",
+    icon: "mdi-film",
+    link: "/admin/films",
+  },
+  {
+    label: "Participants",
+    icon: "mdi-account-group",
+    link: "/admin/participants",
+  },
+  {
+    label: "Actualité",
+    icon: "mdi-newspaper",
+    link: "/admin/actualites",
+  },
+  {
+    label: "En ce moment",
+    icon: "mdi-clock",
+    link: "/admin/moment",
+  },
+  {
+    label: "Anecdotes",
+    icon: "mdi-message-text",
+    link: "/admin/anecdotes",
+  },
+  {
+    label: "Images Film",
+    icon: "mdi-image-multiple",
+    link: "/admin/images",
+  },
+  {
+    label: "Commentaires",
+    icon: "mdi-comment-multiple",
+    link: "/admin/commentaires",
+  },
+]);
+
+const currentTitle = computed(() => {
+  const currentRoute = route.path.split('/').pop();
+  const item = items.value.find(item => item.link.includes(currentRoute));
+  return item ? item.label : "";
+});
+</script>
+
+<style scoped>
+/* Conteneur principal qui gère la mise en page */
+.container {
+  display: flex;
+  flex-direction: row;
+}
+
+/* Barre de navigation */
+.navigation-drawer {
+  width: 250px; /* Fixe une largeur à la barre de navigation */
+  flex-shrink: 0; /* Empêche la barre de navigation de rétrécir */
+}
+
+/* Contenu principal à droite de la navigation */
+.active-page {
+  flex-grow: 1;
+  padding: 20px;
+  overflow-y: auto;
+}
+
+/* Style pour l'élément actif du menu */
+.active-item {
+  background-color: var(--color-button);
+  color: var(--color-text);
+}
+
+.custom-hr {
+  width: 3px; /* Épaisseur du séparateur */
+  background-color: var(--color-button);
+  border: none; /* Supprime la bordure par défaut */
+  height: 100vh; /* Fait toute la hauteur de l'écran */
+
+}
 </style>
