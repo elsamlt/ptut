0b4c7cede78b9f30353382fa9b23fee6909d8d0f
Elsa <144545254+milhetcreat@users.noreply.github.com>
2025-03-30 09:01:15 +0200

    FilmCard okay

--- frontend/src/components/FilmsCard.vue
+++ frontend/src/components/FilmsCard.vue
@@ -1,13 +1,18 @@
 <template>
   <v-card class="mb-4 card">
+    <!-- Bouton en haut à droite -->
+    <v-btn class="btn" size="40px" @click="handleFilmClick(film.idFilm)">
+      <v-icon class="icon">mdi-information</v-icon>
+    </v-btn>
+
     <v-row class="d-flex align-center">
       <v-col cols="3">
         <v-img :src="film.affiche ? film.affiche : defaultImage" aspect-ratio="1"></v-img>
       </v-col>
       <v-col cols="9" class="pa-4">
-        <h3>{{film.titre}}</h3>
-        <p class="details">{{film.annee}} - {{film.duree}} - {{film.genre}}</p>
-        <p><strong>Synopsis :</strong> {{film.synopsis}}</p>
+        <h3>{{ film.titre }}</h3>
+        <p class="details">{{ film.annee }} - {{ film.duree }} - {{ film.genre }}</p>
+        <p><strong>Synopsis :</strong> {{ film.synopsis }}</p>
       </v-col>
     </v-row>
   </v-card>
@@ -22,6 +27,10 @@ const props = defineProps({
 
 const defaultImage = '/images/affiche-default.jpg';
 
+const handleFilmClick = (idFilm) => {
+  console.log("Film sélectionné, ID :", idFilm);
+};
+
 </script>
 
 <style scoped>
@@ -35,5 +44,25 @@ h3{
 
 .card{
   margin-top: 20px;
+}
+
+.btn:hover .icon {
+  transform: scale(0.8);
+  transition: transform 0.3s ease;
+}
+
+/* Position du bouton en haut à droite */
+.btn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: var(--color-button);
+  color: var(--color-text);
+  border-radius: 50px;
+  box-shadow: inset 0 0 5px rgba(255, 255, 255, 0.6);
+}
+
+.icon {
+  font-size: 24px;
 }
 </style>
